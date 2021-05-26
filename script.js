a4bcb005d73f9a43d6c184f1aaef007d89fd5bfc
Raj Prasad <rajeshwarrprasad@yahoo.com>
2021-05-26 09:30:55 +0000

    bug fixes

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
+//This function gets the value of rate, years, interest and calculates the amount payable
 function compute()
 {
     p = document.getElementById("principal").value;
     var principal = document.getElementById("principal").value;
+    // checks to see if the principal amount is greater than zero
+    if(principal <=0){
+        alert("Enter a positive number");
+        //for user friendlyness move cursor to the principal text box
+        document.getElementById("principal").focus();
+
+    }else{
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
     var result = document.getElementById("result");
     var total = parseFloat(principal)+parseFloat(interest);
-    result.innerText="If you deposit "+principal+",at an interest rate of "+rate+"%. You will receive an amount of "+total+",in the year "+year;
+    result.innerText="If you deposit "+principal+",at an interest rate of "+rate+"%. You will receive an amount of "+interest+",in the year "+year;
+    }
+
 
 }
 
