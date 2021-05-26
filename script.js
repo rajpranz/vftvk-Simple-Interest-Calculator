239b94147b7315658e51788a55c37b1f2e5d2dad
Raj Prasad <rajeshwarrprasad@yahoo.com>
2021-05-26 09:52:44 +0000

    highlight the results of calculation

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function compute()
     var year = new Date().getFullYear()+parseInt(years);
     var result = document.getElementById("result");
     var total = parseFloat(principal)+parseFloat(interest);
-    result.innerText="If you deposit "+principal+",at an interest rate of "+rate+"%. You will receive an amount of "+interest+",in the year "+year;
+    result.innerHTML="If you deposit <mark>"+principal+"</mark>,</br>at an interest rate of <mark>"+rate+"</mark>,</br> You will receive an amount of <mark>"+interest+"</mark>,</br>in the year <mark>"+year+"</mark>";
     }
 
 
