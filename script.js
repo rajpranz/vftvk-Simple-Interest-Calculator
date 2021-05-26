27f2283cc8b91714d1ec6d1fa056570d47db383d
Raj Prasad <rajeshwarrprasad@yahoo.com>
2021-05-26 10:12:35 +0000

    Bug fix

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function compute()
     var year = new Date().getFullYear()+parseInt(years);
     var result = document.getElementById("result");
     var total = parseFloat(principal)+parseFloat(interest);
-    result.innerHTML="If you deposit <mark>"+principal+"</mark>,</br>at an interest rate of <mark>"+rate+"</mark>,</br> You will receive an amount of <mark>"+interest+"</mark>,</br>in the year <mark>"+year+"</mark>";
+    result.innerHTML="If you deposit $<mark>"+principal+"</mark>,</br>at an interest rate of <mark>"+rate+"</mark>%,</br> You will receive an amount of $<mark>"+interest+"</mark>,</br>in the year <mark>"+year+"</mark>";
     }
 
 
